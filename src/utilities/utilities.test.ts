5ed74b2948a1a75994e4187927c591f8deb12205
AmyBlank <amy@atlasup.com>
2023-11-12 23:04:06 -0600

    Reducer first draft finished

--- src/utilities/utilities.test.ts
+++ src/utilities/utilities.test.ts
@@ -1,10 +1,57 @@
-import {createUpdateAction} from "./utilities";
+import {createUpdateAction, genericReducer} from "./utilities";
 
 describe('utilities', ()=> {
-  it('should create an update action', () => {
-    expect(createUpdateAction('street_address', '1234 5th St')).toEqual({
-      type: 'update_street_address',
-      payload: '1234 5th St'
-    })
+  describe('action creator', () => {
+    it('should create an update action', () => {
+      expect(createUpdateAction('street_address', '1234 5th St')).toEqual({
+        type: 'update_street_address',
+        payload: '1234 5th St'
+      });
+    });
+  });
+  describe('reducer', () => {
+    const initialState = {
+      street_address: '1234 5th St.',
+      city: 'Anywhere',
+      state: 'MD',
+      zip: 12345
+    };
+    it('should update state in response to an update action', () => {
+      const action = {type: 'update_city' as const, payload: 'Somewhere'};
+      const state = genericReducer(initialState, action)
+      expect(state).toEqual({
+        street_address: '1234 5th St.',
+        city: 'Somewhere',
+        state: 'MD',
+        zip: 12345,
+        isChanged: true
+      });
+    });
+    it('should throw useful error when called without a starting state', () => {
+      const action = {type: 'update_city', payload: 'Somewhere'};
+      // @ts-expect-error
+      expect(() => genericReducer(undefined, action)).toThrow('No initial state provided to reducer')
+    });
+    it('should throw useful error when called without an action', () => {
+      // @ts-expect-error
+      expect(()=>genericReducer(initialState)).toThrow('Without an action, we don\'t know how to update the state')
+    });
+    it('should throw useful error when update action does not match a property of the state', () => {
+      const action = {type: 'update_random_prop' as const, payload: 'oops'}
+      expect(()=> genericReducer(initialState, action)).toThrow('Can\'t update property "random_prop" because this is not a valid property on the state.')
+    });
+    it('should overwrite entire state in response to an initialize action', () => {
+      const action = {
+        type: 'initialize' as const,
+        payload: {
+          street_address: '10456 Pruitt Road',
+          city: 'Kalamazoo',
+          state: 'MI',
+          zip: 67890
+        }
+      }
+      expect(genericReducer(initialState, action))
+        .toEqual({...action.payload, isChanged: false})
+    });
   });
 });
--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,9 +1,3 @@
-
-type AnyAction = {
-  type: string,
-  payload: unknown
-}
-
 /*  See
     https://medium.com/better-programming/building-typescript-types-for-the-usereducer-hook-c19e7690af6c
     for more about these types
@@ -22,9 +16,38 @@ export type UpdateActionMap<SourceType extends Record<string, unknown>> = {
 // Create the map and get the types from the keys
 export type UpdateActions<SourceType extends Record<string, any>> = UpdateActionMap<SourceType>[keyof SourceType];
 
+export type InitializeAction = {
+  type: 'initialize',
+  payload: Record<string, unknown>
+}
+
 export const createUpdateAction = <FieldName extends string, PayloadType = unknown>(field: FieldName, payload: PayloadType): UpdateAction<FieldName, PayloadType> => {
   return {
     type: `update_${field}`,
     payload
   }
+}
+
+type CreatedAction = ReturnType<typeof createUpdateAction>
+
+type UpdateOrInitializeAction = CreatedAction | InitializeAction
+
+export const genericReducer = <S extends Record<string, unknown>, A extends UpdateOrInitializeAction>(state: S, action: A) => {
+  if (!action) {
+    throw new Error('Without an action, we don\'t know how to update the state.')
+  }
+  if (action.type.startsWith('update_')) {
+    if (!state) {
+      throw new Error('No initial state provided to reducer.')
+    }
+    const prop_name = action.type.replace('update_', '')
+    if (!state.hasOwnProperty(prop_name)) {
+      throw new Error('Can\'t update property "random_prop" because this is not a valid property on the state.')
+    }
+    return {...state, [prop_name]: action.payload, isChanged: true}
+  }
+  if (action.type === 'initialize') {
+    return {...action.payload, isChanged: false}
+  }
+  return { ...state }
 }
